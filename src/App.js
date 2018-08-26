84dee4b848e2ca98a9a957e2bec8606b0da09b57
Tankcaster <tankcaster@gmail.com>
2018-08-26 10:03:24 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Game from './Pages/Game.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Game />
+      </div>
+    );
+  }
+}
+
+export default App;
